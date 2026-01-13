af007b4dc0dc1d8e5691b7c00a91c20dee0d8781
anandhupp73 <anandhakrishnanpp73@gmail.com>
2026-01-13 12:49:47 +0530

    env

--- PcforYou/PcforYou/settings.py
+++ PcforYou/PcforYou/settings.py
@@ -180,12 +180,11 @@ MESSAGE_TAGS = {
 
 SITE_ID = 2
 
-# --- Email backend for local development ---
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anandhupp67@gmail.com'
-EMAIL_HOST_PASSWORD = 'bxlv imup dmca fbgz'  # Use App Password if Gmail
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD") 
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
